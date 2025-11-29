58eaa97f970235b4c552262904bf3925587af16f
ktc2241010004 <ktc2241010004@edu.kyoto-tech.ac.jp>
2025-11-29 18:08:07 +0900

    1129

--- Project1/Project1/DisplayClear.cpp
+++ Project1/Project1/DisplayClear.cpp
@@ -2,48 +2,55 @@
 
 class DescriptorHeap{
 public:
-	bool create(ID3D12Device* device) {
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+	ID3D12DescriptorHeap* rtvHeap;
+
+	void create(ID3D12Device& device) {
 		// RTVディスクリプタヒープの作成例
-		D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+		//D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
 		rtvHeapDesc.NumDescriptors = 2;  // スワップチェーンのバッファ数
 		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
 		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 
-		ID3D12DescriptorHeap* rtvHeap;
-		device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
+		//ID3D12DescriptorHeap* rtvHeap;
+		device.CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
 	}
 };
 
 class RenderTarget {
 public:
-	bool create() {
+	void create(IDXGISwapChain3& swapChain, ID3D12Device& device, DescriptorHeap& heap) {
 		// レンダーターゲット作成例
 		ID3D12Resource* renderTargets[2];  // スワップチェーンでバッファが二つあるのでダブルバッファで
+
+		D3D12_DESCRIPTOR_HEAP_TYPE heapType = heap.rtvHeapDesc.Type;
 
 		for (int i = 0; i < 2; i++)
 		{
 			// スワップチェーンからバッファを取得
-			swapChain->GetBuffer(i, IID_PPV_ARGS(&renderTargets[i]));
+			swapChain.GetBuffer(i, IID_PPV_ARGS(&renderTargets[i]));
 
 			// RTVハンドルの計算
-			D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
-			rtvHandle.ptr += i * rtvDescriptorSize;
+			D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = heap.rtvHeap->GetCPUDescriptorHandleForHeapStart();
+			rtvHandle.ptr += device.GetDescriptorHandleIncrementSize(heapType); // i * は必要？.
 
 			// RTVの作成
-			device->CreateRenderTargetView(renderTargets[i], nullptr, rtvHandle);
+			device.CreateRenderTargetView(renderTargets[i], nullptr, rtvHandle);
 		}
 	}
 };
 
 class CommandAllocator {
 public:
-	void test(ID3D12Device* device) {
+	ID3D12CommandAllocator* commandAllocators[2];
+
+	void test(ID3D12Device& device) {
 		// コマンドアロケータ作成例
-		ID3D12CommandAllocator* commandAllocators[2];
+		//ID3D12CommandAllocator* commandAllocators[2];
 
 		for (int i = 0; i < 2; i++)
 		{
-			device->CreateCommandAllocator(
+			device.CreateCommandAllocator(
 				D3D12_COMMAND_LIST_TYPE_DIRECT,
 				IID_PPV_ARGS(&commandAllocators[i])
 			);
@@ -53,13 +60,13 @@ public:
 
 class CommandList {
 public:
-	void test(ID3D12Device* device, ID3D12CommandAllocator* commandAllocators) {
+	void test(ID3D12Device& device, CommandAllocator& commandAllocator, IDXGISwapChain3* swapChain) {
 		// コマンドリスト作成例
 		ID3D12GraphicsCommandList* commandList;
-		device->CreateCommandList(
+		device.CreateCommandList(
 			0,
 			D3D12_COMMAND_LIST_TYPE_DIRECT,
-			commandAllocators[currentBackBuffer],
+			commandAllocator.commandAllocators[swapChain->GetCurrentBackBufferIndex()],
 			nullptr,
 			IID_PPV_ARGS(&commandList)
 		);
