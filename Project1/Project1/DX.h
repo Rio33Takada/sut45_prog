ad6eccfb2625cf9e5bad0434fdb5ea4a59e3b59f
Kobamizu <ktc24a31a0019@edu.kyoto-tech.ac.jp>
2026-01-22 17:28:11 +0900

    0122

--- Project1/Project1/DX.h
+++ Project1/Project1/DX.h
@@ -13,4 +13,41 @@ public:
 	IDXGIAdapter* HWadapter;
 	IDXGISwapChain3* sc;
 	ID3D12Device* d3d12d;
+
+	class DXGI {
+	public:
+		IDXGIFactory4* CreateDXGIFactory();
+	};
+
+	class adapter {
+	public:
+		IDXGIAdapter* GetHardwareAdapter(IDXGIFactory4* factory);
+	};
+
+	class commandQueue {
+	public:
+		ID3D12CommandQueue* CreateCommandQueue(ID3D12Device* device);
+	};
+
+	class device {
+	public:
+		ID3D12Device* CreateD3D12Device(IDXGIAdapter* adapter);
+	};
+
+	class swapChain {
+	public:
+		IDXGISwapChain3* CreateSwapChain(IDXGIFactory4* factory, ID3D12CommandQueue* commandQueue, HWND hwnd);
+	};
+
+	class debugLayer {
+	public:
+		void EnableDebugLayer();
+	};
+
+	DXGI dxgic;
+	adapter adpc;
+	commandQueue cqc;
+	device devc;
+	swapChain scc;
+	debugLayer dlc;
 };
